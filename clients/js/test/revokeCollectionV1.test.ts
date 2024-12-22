8b5dc8b6780f88da599e1056658c94db8297efeb
Kartik Soneji <kartiksoneji@rocketmail.com>
2024-12-22 15:10:45 +0530

    Add self-revoke collection delegate tests

--- clients/js/test/revokeCollectionV1.test.ts
+++ clients/js/test/revokeCollectionV1.test.ts
@@ -48,4 +48,41 @@ NON_EDITION_TOKEN_STANDARDS.forEach((tokenStandard) => {
     // Then the metadata delegate record was deleted.
     t.false(await umi.rpc.accountExists(metadataDelegateRecord));
   });
+
+  test(`it can self-revoke a collection delegate for a ${tokenStandard}`, async (t) => {
+    // Given an asset with an approved collection delegate.
+    const umi = await createUmi();
+    const updateAuthority = generateSigner(umi);
+    const { publicKey: mint } = await createDigitalAssetWithToken(umi, {
+      authority: updateAuthority,
+      tokenStandard: TokenStandard[tokenStandard],
+    });
+	const collectionDelegateSigner = generateSigner(umi);
+    const collectionDelegate = collectionDelegateSigner.publicKey;
+    await delegateCollectionV1(umi, {
+      mint,
+      authority: updateAuthority,
+      delegate: collectionDelegate,
+      tokenStandard: TokenStandard[tokenStandard],
+    }).sendAndConfirm(umi);
+    const [metadataDelegateRecord] = findMetadataDelegateRecordPda(umi, {
+      mint,
+      delegateRole: MetadataDelegateRole.Collection,
+      delegate: collectionDelegate,
+      updateAuthority: updateAuthority.publicKey,
+    });
+    t.true(await umi.rpc.accountExists(metadataDelegateRecord));
+
+    // When we revoke the collection delegate.
+    await revokeCollectionV1(umi, {
+      mint,
+      authority: collectionDelegateSigner,
+	  updateAuthority: updateAuthority.publicKey,
+      delegate: collectionDelegate,
+      tokenStandard: TokenStandard[tokenStandard],
+    }).sendAndConfirm(umi);
+
+    // Then the metadata delegate record was deleted.
+    t.false(await umi.rpc.accountExists(metadataDelegateRecord));
+  });
 });
